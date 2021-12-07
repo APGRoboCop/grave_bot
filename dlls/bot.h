e5ada99435f9f5941078b67e69a4cf197aaa9717
Ryan Hunter <robocop@lycos.co.uk>
2021-12-07 22:03:55 +0000

    Update bot.h

--- dlls/bot.h
+++ dlls/bot.h
@@ -17,9 +17,9 @@ typedef int BOOL;
 #endif
 
 #ifndef METAMOD_BUILD
-   #define GRAVEBOT_VERSION "0.4b3-APG" // non-metamod
+   #define GRAVEBOT_VERSION "0.4b4-APG" // non-metamod
 #else
-   #define GRAVEBOT_VERSION "0.4b3_mm-APG" // metamod
+   #define GRAVEBOT_VERSION "0.4b4_mm-APG" // metamod
 #endif
 
 
